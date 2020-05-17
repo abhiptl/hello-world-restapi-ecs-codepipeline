7bad231459b7c905334d4d78dee807b5772f1c87
Abhishek Patel <abhishek.patel@conns.com>
2020-05-17 15:26:53 +0530

    Initial commit

---
+++ src/test/java/com/abhishek/microservices/ecs/helloworldrestapicodepipeline/HelloWorldRestApiCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.microservices.ecs.helloworldrestapicodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldRestApiCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
